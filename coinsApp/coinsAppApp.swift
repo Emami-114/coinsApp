3cefe085ae29b0feb8a75c286f6deb41a8027bc2
Emami.114 <emami.114@outlook.de>
2023-08-08 17:37:41 +0200

    Initial Commit

---
+++ coinsApp/coinsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  coinsAppApp.swift
+//  coinsApp
+//
+//  Created by Ecc on 08.08.23.
+//
+
+import SwiftUI
+
+@main
+struct coinsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
